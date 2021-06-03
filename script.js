d71113835e78c0ef9d94dce37e56840b6b45af9f
Aka.Kiril <62017663+UserKiril@users.noreply.github.com>
2021-06-03 10:32:19 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const searchInput = document.querySelector(".search");
+const App_ID = 'a6f275c1c5fcf63200ec318f00acdbc0';
+const DEFAULT1 = '-_-';
+
+const location1 = document.querySelector("#city");
+const temp = document.querySelector("#temp");
+const info = document.querySelector("#info");
+const humidity = document.querySelector("#humidity");
+const windSpeed = document.querySelector("#wind-speed");
+
+
+searchInput.addEventListener('change', (e) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${App_ID}&units=metric`)
+        .then(async res => {
+            const data = await res.json();
+            // console.log(data);
+            location1.innerHTML = `${data.name || DEFAULT1}`; 
+            temp.innerHTML = `Nhiệt Độ: ${data.main.temp || DEFAULT1}°C`;
+            windSpeed.innerHTML = `Speed: ${data.wind.speed || DEFAULT1} km/h`;
+            humidity.innerHTML = `Độ Ẩm: ${data.main.humidity || DEFAULT1}%`;
+            info.innerHTML = `${data.weather[0].main || DEFAULT1}`;
+        });
+})
+
+var typed = new Typed('#type', {
+    strings:["Hi I'm Kiril","I'm Newbie","Nice to meet you ~"],
+    typeSpeed :120,
+    backSpeed: 40,
+    loop:true,
+    showCursor: false,
+  });
+
+  console.log(typed)
